06ee6fcfc92db989440422ec35e4c0b43416d157
Jimena-7 <jimejardi07@gmail.com>
2020-10-27 23:24:47 -0300

    Commit 3

--- app/src/main/java/br/senai/sc/modelo/Produto.java
+++ app/src/main/java/br/senai/sc/modelo/Produto.java
@@ -2,13 +2,31 @@ package br.senai.sc.modelo;
 
 import androidx.annotation.NonNull;
 
-public class Produto {
+import java.io.Serializable;
+
+public class Produto implements Serializable {
+    private int id;
     private String Nome;
     private float Valor;
+
+
+    public Produto(int id, String nome, float valor) {
+        this.id = id;
+        this.Nome = nome;
+        this.Valor = valor;
+    }
 
     public Produto(String nome, float valor) {
         Nome = nome;
         Valor = valor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getNome() {
